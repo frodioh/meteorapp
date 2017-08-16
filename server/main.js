caee363f4adc921c785fd277586656a9c26620e7
Rodion <rodion@bobrick.su>
2017-08-16 21:28:46 +0500

    go home

--- server/main.js
+++ server/main.js
@@ -1,16 +1,29 @@
 if (Posts.find().count() === 0) {
     Posts.insert({
         title: 'Диоген',
-        url: 'http://sachagreif.com/introducing-telescope/'
+        url: 'http://sachagreif.com/introducing-telescope/',
+        description: 'Диоген синопский - киник',
+        flagged: false
     });
 
     Posts.insert({
-        title: 'Meteor',
-        url: 'http://meteor.com'
+        title: 'Лорем ипсум когито ерго сум',
+        url: 'http://meteor.com',
+        description: 'Рыбный текст - это очень хорошо',
+        flagged: false
     });
 
     Posts.insert({
-        title: 'The Meteor Book',
-        url: 'http://themeteorbook.com'
+        title: 'И ты, Брут!',
+        url: 'http://themeteorbook.com',
+        description: 'И я, Цезарь..',
+        flagged: false
     });
+
+    Posts.insert({
+        title: 'Метеор - так себе фреймворк',
+        url: 'http://yandex.ru',
+        description: 'Реактивность - полный бред',
+        flagged: true
+    })
 }
