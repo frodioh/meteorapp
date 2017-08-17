8e53dad60735fcd362536463794b6755462e4c55
Rodion <rodion@bobrick.su>
2017-08-17 17:43:39 +0500

    added recipes collection

---
+++ collections/recipes.js
@@ -0,0 +1,31 @@
+//Создание коллекции
+Recipes = new Mongo.Collection('recipes');
+
+//Создание схемы для этой коллекции
+RecipeSchema = new SimpleSchema({
+    name: {
+        type: String,
+        label: "Имя"
+    },
+    desc: {
+        type: String,
+        label: "Описание"
+    },
+    author: {
+        type: String,
+        label: "Автор",
+        autoValue: function() {
+            return this.userId
+        }
+    },
+    createdAt: {
+        type: Date,
+        label: "Создан",
+        autoValue: function() {
+            return new Date()
+        }
+    }
+});
+
+//Привязка схемы к коллекции
+Recipes.attachSchema(RecipeSchema);
