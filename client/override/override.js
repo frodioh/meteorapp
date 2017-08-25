60cebd3ca99b93abfbc5db8ce30666bdf61194fc
Rodion <rodion@bobrick.su>
2017-08-25 19:50:41 +0500

    auto school

---
+++ client/override/override.js
@@ -0,0 +1,4 @@
+Template['override-atForm'].replaces('atForm');
+Template['override-atPwdForm'].replaces('atPwdForm');
+Template['override-atPwdFormBtn'].replaces('atPwdFormBtn');
+Template['override-atTextInput'].replaces('atTextInput');
