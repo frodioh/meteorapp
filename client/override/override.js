e9e83f9e251f5b3002705a4e7b4632b5969a195e
Rodion <rodion@bobrick.su>
2017-08-28 19:58:39 +0500

    fixed placeholders

--- client/override/override.js
+++ client/override/override.js
@@ -1,4 +1,5 @@
 Template['override-atForm'].replaces('atForm');
 Template['override-atPwdForm'].replaces('atPwdForm');
+Template['override-atTitle'].replaces('atTitle');
 Template['override-atPwdFormBtn'].replaces('atPwdFormBtn');
 Template['override-atTextInput'].replaces('atTextInput');
---
+++ server/accounts/accounts.js
@@ -0,0 +1,7 @@
+// Generate user initials
+// Accounts.onCreateUser((options, user) => {
+//     const { first_name, last_name } = user.services.facebook;
+//     user.initials = first_name[0].toUpperCase() + last_name[0].toUpperCase();
+//     // Don't forget to return the new user object at the end!
+//     return user;
+// });
